014be78d0332db138f999174c22ecbc1e94a0873
konbra99 <56540180+konbra99@users.noreply.github.com>
2020-01-25 11:12:18 +0100

    Update StrategiaUzytkownika.java

--- src/com/company/StrategiaUzytkownika.java
+++ src/com/company/StrategiaUzytkownika.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class StrategiaUzytkownika implements StrategiaDostepu {
     @Override
     public void zamawiaj(String s) {
         String wybrany = s;
-        System.out.println("klient zamowil ksiazke " + wybrany);
+        try {
+            ArrayList<String> kolumny = new ArrayList<>();
+            kolumny.add("id_uzytkownika");
+            kolumny.add("id_ksiazki");
+            kolumny.add("status");
+
+            String[] war = s.split(":");
+            ArrayList<String> wartosci = new ArrayList<>();
+            wartosci.add(GlowneOkno.id.toString());
+            wartosci.add(war[0]);
+            wartosci.add("'" + "zamowiona'");
+            GlowneOkno.polaczenie.wstawianie(new QueryBuilder()
+                    .insert("Lista_wypozyczen", kolumny, wartosci)
+                    .getQuery().toString());
+        } catch (SQLException ignored) {
+        }
     }
 
     @Override
