69d12a2fb9b6e9f320a2f08210b60b55ddc31bd3
konbra99 <56540180+konbra99@users.noreply.github.com>
2020-01-25 11:11:17 +0100

    Update StrategiaAdmina.java

--- src/com/company/StrategiaAdmina.java
+++ src/com/company/StrategiaAdmina.java
@@ -1,10 +1,28 @@
 package com.company;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class StrategiaAdmina implements StrategiaDostepu {
     @Override
     public void zamawiaj(String s) {
-        String wybrany = s;
-        System.out.println("admin zamowil ksiazke " + wybrany);
+        try {
+            ArrayList<String> kolumny = new ArrayList<>();
+            kolumny.add("id_uzytkownika");
+            kolumny.add("id_ksiazki");
+            kolumny.add("status");
+
+            String[] war = s.split(":");
+            ArrayList<String> wartosci = new ArrayList<>();
+            wartosci.add(GlowneOkno.id.toString());
+            wartosci.add(war[0]);
+            wartosci.add("'" + "zamowiona'");
+            int count = GlowneOkno.polaczenie.wstawianie(new QueryBuilder()
+                    .insert("Lista_wypozyczen", kolumny, wartosci)
+                    .getQuery().toString());
+            System.out.println("zmieniono " + count);
+        } catch (SQLException ignored) {
+        }
     }
 
     @Override
