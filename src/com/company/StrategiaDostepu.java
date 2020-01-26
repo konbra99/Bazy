14283df746f85f0b6a4718cb916b6d54eb907533
konbra99 <56540180+konbra99@users.noreply.github.com>
2020-01-26 12:40:50 +0100

    Update StrategiaDostepu.java

--- src/com/company/StrategiaDostepu.java
+++ src/com/company/StrategiaDostepu.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.sql.SQLException;
+
 public interface StrategiaDostepu {
     void zamawiaj(String s);
     void uzytkownicy();
+    void start() throws InterruptedException;
 }
